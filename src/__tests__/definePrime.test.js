efc6ca4fb46d4336929af63664ae0fb50f156ff4
Сергей Викторович Князев <113102209+Knyazew36@users.noreply.github.com>
2023-01-15 12:14:32 +0300

    Initial commit

---
+++ src/__tests__/definePrime.test.js
@@ -0,0 +1,11 @@
+import { definePrime } from "../definePrime.js";
+
+describe("test for definePrime function", () => {
+  const simple = 17;
+  const complex = 18;
+  it("should operate correctly with simple,complex and invalid number", () => {
+    expect(definePrime(simple)).toBe(`Число ${simple} - простое число`);
+    expect(definePrime(complex)).toBe(`Число ${complex} - составное число`);
+    expect(definePrime(1001)).toBe("Данные неверны");
+  });
+});
